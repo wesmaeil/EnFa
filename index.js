8ee1f0bf276a699a19fffbf8b6914f46cc3599ce
wesmaeil <53255972+wesmaeil@users.noreply.github.com>
2019-07-24 13:19:06 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+code = {
+"q":"ض", "w":"ص", "e":"ث", "r":"ق", "t":"ف", "y":"غ", "u":"ع", "i":"ه", "o":"خ", "p":"ح",
+"[":"ج", "]":"چ", "a":"ش", "s":"س", "d":"ی", "f":"ب", "g":"ل", "h":"ا", "H":"آ", "j":"ت",
+"J":"ـ", "k":"ن", "l":"م",
+";":"ک",
+"'":"گ",
+"\\":"پ", "z":"ظ", "x":"ط", "c":"ز", "v":"ر", "b":"ذ", "n":"د", "m":"ئ", ",":"و",
+"?":"؟", "C":"ژ",
+
+
+
+};
+
+
+var decodedText = '';
+$(".box").val("");
+$(".btn-d").on("click", function() {
+
+	var inputText = $(".box-input").val();
+
+
+	for (var i = 0; i < inputText.length; i++) {
+
+    var letter = inputText.charAt(i);
+        if (letter in code) {
+        decodedText = decodedText + code[letter];
+        } else {
+          decodedText = decodedText + letter;
+        }
+
+      $(".box-output").removeAttr("disabled");
+      $(".box-output").addClass("r-to-l");
+      $(".box-output").val(decodedText);
+      $(".box-output").attr("placeholder", "");
+    }
+
+});
+
+
+
+$(".btn-r").on("click", function () {
+$(".box").val("");
+$(".box-output").attr("disabled", true);
+$(".box-output").attr("placeholder", "Your output will show here :)");
+$(".box-output").removeClass("r-to-l");
+decodedText = "";
+
+
+});
